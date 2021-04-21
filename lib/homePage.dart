9aaaaa17682f9549dd6f5d5c6f0b91ce01beb09f
shivam05241 <shivam05241@gmail.com>
2021-04-21 13:44:51 +0530

    custom widget added

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -17,20 +18,83 @@ class _HomePageState extends State<HomePage> {
       body: Column(
         children: [
           Expanded(
+            flex: 3,
             child: Row(
               children: [
-                CustomContainer(colour: secondryColor),
-                CustomContainer(colour: secondryColor),
+                CustomContainer(
+                  flex: 1,
+                  colour: secondryColor,
+                  customChild: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        FontAwesomeIcons.mars,
+                        size: 80,
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        "MALE",
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Color(0xFF8D8E98),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                CustomContainer(
+                  flex: 1,
+                  colour: secondryColor,
+                  customChild: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        FontAwesomeIcons.venus,
+                        size: 80,
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        "Female",
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Color(0xFF8D8E98),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
-          CustomContainer(colour: secondryColor),
+          CustomContainer(
+            colour: secondryColor,
+            flex: 3,
+          ),
           Expanded(
+            flex: 3,
             child: Row(
               children: [
-                CustomContainer(colour: secondryColor),
-                CustomContainer(colour: secondryColor),
+                CustomContainer(
+                  colour: secondryColor,
+                  flex: 1,
+                ),
+                CustomContainer(
+                  colour: secondryColor,
+                  flex: 1,
+                ),
               ],
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              margin: EdgeInsets.only(top: 8),
+              color: Color(0xFFEB1555),
+              child: Center(child: Text("CALCULATE")),
             ),
           ),
         ],
@@ -40,17 +104,21 @@ class _HomePageState extends State<HomePage> {
 }
 
 class CustomContainer extends StatelessWidget {
-  CustomContainer({@required this.colour});
+  CustomContainer({@required this.colour, this.customChild, this.flex});
   final Color colour;
+  final Widget customChild;
+  int flex;
   @override
   Widget build(BuildContext context) {
     return Expanded(
+      flex: flex,
       child: Container(
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(8),
           color: colour,
         ),
         margin: EdgeInsets.all(8),
+        child: customChild,
       ),
     );
   }
